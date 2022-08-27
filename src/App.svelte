b460df8741723e4e794e0121a6c62ddcbb10cfbc
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2022-08-27 20:59:47 +0100

    added few more components to the application.

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,12 @@
 <script>
 	import Hero from './hero.svelte'
+	import Feature from './feature.svelte';
+	import Cta from './cta.svelte';
 </script>
 
 <Hero />
+<Feature />
+<Cta />
 
 <style global>
   @tailwind base;
